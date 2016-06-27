3190342f6559d982d58e0b122d4d4c4781e549a8
Max <m_mahlke@yahoo.de>
2016-06-27 22:26:16 +0200

    =Bugfix

--- SolarSystemOrbiter/calc/all_in_one.py
+++ SolarSystemOrbiter/calc/all_in_one.py
@@ -111,7 +111,7 @@ def leap_frog(nsteps, distance, angle):
 
     # Use distance and anlge to calculate inital positions x_0, y_0 and velocities
     d = distance * AU
-    a = angle * math.pi / 180
+    a = angle * np.pi / 180
     v = np.sqrt(G*M/d)
 
     x_0 = d*np.sin(a)
