e9c7a68dd1ee0349457bee65638c970bd6efc384
Max Mahlke <max@priv-3a-181.physik.rwth-aachen.de>
2016-12-20 17:54:35 +0100

    =Code cleanup

--- SolarSystemOrbiter/calculations.py
+++ SolarSystemOrbiter/calculations.py
@@ -63,7 +63,6 @@ def hohmann(steps, origin, target, mode, movie):
     v_x_0 -= G*M*x_0/r(x_0, y_0)**3 * delta/2
     v_y_0 -= G*M*y_0/r(x_0, y_0)**3 * delta/2
 
-
     # Maneuvers at target planet. We can do nothing, perform an GAM or use a second impulse
     # to insert the spacecraft into the orbit of the target planet. If GAM, the SoI is relevant.
     sphere_of_influence = r_1*0.0001
@@ -109,11 +108,7 @@ def hohmann(steps, origin, target, mode, movie):
                     # If GAM has not been performed yet
                     if not gam_impulse:
                         print(u'\U0001f680 : Reached Sphere of Influence of Target Planet.\n    Performing Gravity Assist Maneuver..')
-                       # print((x_1, target_x), (y_1, target_y))
                         v_x_0, v_y_0 = GAM((x_1, y_1), (target_x, target_y), (v_x_1, v_y_1), target, movie)
-                        #plt.plot((x_1, target_x), (y_1, target_y))
-
-                       # x_0, y_0, v_x_0, v_y_0 = GAM((x_1, y_1), (target_x, target_y), (v_x_1, v_y_1), r_1, sphere_of_influence)
                         gam_impulse = True # impulse has been provided now
                 elif mode == 1: # Orbit injection
                     # If we want to insert the spacecraft into the orbit of the target planet
@@ -161,8 +156,6 @@ def planets(steps, semi_major, eccentricity, angle):
         y_1 = step_position(y_0, v_y_0, delta)
         v_x_1 = step_velocity(v_x_0, x_1, y_1, delta, M)
         v_y_1 = step_velocity(v_y_0, y_1, x_1, delta, M)
-        #print('leap')
-        #print('planet vx/xy: ', v_x_1, v_y_1)
         x_0 = x_1
         y_0 = y_1
         v_x_0 = v_x_1
@@ -175,7 +168,7 @@ def GAM(position_spacecraft, position_planet, velocity_spacecraft, target, movie
 	# We switch from heliocentric reference system to system of planet
 
 	G = 6.67e-11    			# gravitational constant
-	M = 1.998e30					# mass of Sun
+	M = 1.998e30				# mass of Sun
 
 	# x and y of spacecraft, heliocentric
 	x_sc = position_spacecraft[0]
@@ -194,14 +187,6 @@ def GAM(position_spacecraft, position_planet, velocity_spacecraft, target, movie
 	vx_p = -np.cos(np.arctan(x_p/y_p))*v_p
 	vy_p = np.sin(np.arctan(x_p/y_p))*v_p
 
-	#print('Heliocentric distance Planet - Spacecraft')
-	# Distance between spacecraft and planet
-	#print(np.sqrt((x_p-x_sc)**2+(y_p-y_sc)**2))
-
-	#print('Heliocentric relative velocities Planet - Spacecraft')
-	#print(x_p,x_sc, y_p, y_sc)
-	#print(vx_p,vx_sc, vy_p, vy_sc)
-
 	# x and y of spacecraft, planet-centric
 	x_sc = x_sc - x_p
 	y_sc = y_sc - y_p
@@ -209,16 +194,6 @@ def GAM(position_spacecraft, position_planet, velocity_spacecraft, target, movie
 	# vx and vy of spacecraft, planet-centric
 	vx_sc = vx_sc - vx_p
 	vy_sc = vy_sc - vy_p
-
-
-
-	#print('Planet-centric distance Planet - Spacecraft')
-	# Distance between spacecraft and planet
-	#print(np.sqrt(x_sc**2+y_sc**2))
-
-	#print('Planet-centric relative velocities Planet - Spacecraft')
-	#print(vx_sc, vy_sc)
-
 
 	# "Switch gravity" from Sun to swing-by planet.
 	# Integration
@@ -238,8 +213,6 @@ def GAM(position_spacecraft, position_planet, velocity_spacecraft, target, movie
 			y_sc_1 = step_position(y_sc, vy_sc, delta)
 			v_x_1 = step_velocity(vx_sc, x_sc_1, y_sc_1, delta, M)
 			v_y_1 = step_velocity(vy_sc, y_sc_1, x_sc_1, delta, M)
-			#print('leap')
-			#print('planet vx/xy: ', v_x_1, v_y_1)
 			x_sc = x_sc_1
 			y_sc = y_sc_1
 			vx_sc = v_x_1
@@ -257,50 +230,19 @@ def GAM(position_spacecraft, position_planet, velocity_spacecraft, target, movie
 		plt.axis('equal')
 		if not movie:
 			f.show()
-#		else:
-#			plt.savefig('gam.png')
-#			plt.gcf().clf()
+		else:
+			plt.savefig('gam.png')
+			plt.gcf().clf()
 
 	# Transform velocities back to heliocentric frame of reference
 	# Planet's movement not considered...
 
 	# x and y of spacecraft, helio-centric again
-	x_sc = x_sc + x_p
-	y_sc = y_sc + y_p
+	x_sc += x_p
+	y_sc += y_p
 
 	# vx and vy of spacecraft, helio-centric again
-	vx_sc = vx_sc + vx_p
-	vy_sc = vy_sc + vy_p
-
-	#print('ratio before/after velocity:', np.sqrt(vy_sc**2+vx_sc**2)/np.sqrt(velocity_spacecraft[0]**2+velocity_spacecraft[1]**2))
+	vx_sc += vx_p
+	vy_sc += vy_p
 	return vx_sc, vy_sc
-	#return x_sc, y_sc, vx_sc, vy_sc
-
-	# From incoming angle and velocity, calculate change in velocites due to gravitational interaction
-    # apply to spacecraft, set gam_impulse to True (It has happened)
-
-    # Relative motion section http://www.bogan.ca/orbits/gravasst/gravasst.html
-    # Get relative velocity planet - spacecraft
-
-# for planet
-
-	if False:
-		# Get relative velocity of planet and spacecraft
-		v_planet = np.sqrt(G*M/r_1)
-		vx_planet = -np.cos(np.arctan(target_x/target_y))*v_planet
-		vy_planet = np.sin(np.arctan(target_x/target_y))*v_planet
-	#	print('gam')
-	#	print('planet vx/vy: ',vx_planet, vy_planet)
-	#	print('spacecraft vx/vy: ', v_x_0, v_y_0)
-		v_rel_x = vx_planet - v_x_0
-		v_rel_y = vy_planet - v_y_0
-	#	print('relative vx_vy: ', v_rel_x, v_rel_y)
-		# new angle of velocity vector
-		v_alpha = np.arctan(v_rel_y/v_rel_x)
-	#	print(v_alpha*180./np.pi)
-		v_amp = np.sqrt(v_planet**2+v_x_0*+2+v_y_0**2-2*v_planet*np.sqrt(v_x_0*+2+v_y_0**2)*np.cos(v_alpha))
-	#	print(v_amp)
-	#	print(np.sqrt(v_x_0*+2+v_y_0**2))
-		v_x_0 = v_amp*np.cos(v_alpha)
-		v_y_0 = v_amp*np.sin(v_alpha)
-
+	
